bba7d00efbb2b6f414f504c592bc53f7bfee463e
chanpen0811 <jan1574p@gmail.com>
2024-02-13 14:14:01 +0700

    up

--- src/components/Wonder/WonderView.vue
+++ src/components/Wonder/WonderView.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-3 mb-3" v-for="item in allWonders" :key="item.id">
                 <div class="card shadow" style="height:18rem;">
-                    <img :src="item.imageUrl" class="card-img-top" alt="wonders">
+                    <img :src="item.imageUrl" class="card-img-top" height="200px" alt="wonders">
                     <div class="card-body">
                         <h5 class="card-title">{{ item.name }}</h5>
                         <p class="card-text">Location - {{ item.location }}</p>
