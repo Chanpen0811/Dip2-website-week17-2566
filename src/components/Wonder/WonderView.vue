2ada0e7e36c9706698d32872d06cc38c9386966c
chanpen0811 <jan1574p@gmail.com>
2024-02-13 14:29:20 +0700

    up

--- src/components/Wonder/WonderView.vue
+++ src/components/Wonder/WonderView.vue
@@ -5,7 +5,7 @@
                 <div class="card shadow" style="height:18rem;">
                     <img :src="item.imageUrl" class="card-img-top" height="200px" alt="wonders">
                     <div class="card-body">
-                        <h5 class="card-title">{{ item.name }} {{ item.price }} </h5>
+                        <span class="card-title fs-4">{{ item.name }} {{ item.price }} </span>
                         <p class="card-text">Location - {{ item.location }}</p>
                     </div>
                 </div>
